9b1a28b8b5bc4c70198ed255b7e2aaef144401b2
prsndp <prasandeepp@gmail.com>
2022-10-23 18:11:30 +0530

    form changes done in app.js

--- react-dev/src/App.js
+++ react-dev/src/App.js
@@ -1,25 +1,35 @@
 // import React from 'react';
 import React, {useState} from 'react';
 import './App.css';
-import Student from './Student';
-
-
 function App()
 {
+  const [name, setName] = useState("");
+  const [tnc, setTnc ] = useState(false);
+  const [interest, setInterest] = useState("");
+  function getFormData(e)
+  {
+    console.warn(name, tnc, interest);
+    e.preventDefault()
+  }
   return (
  <div className="App">
   <h1>Handle Forms in React</h1>
-  <form>
-    <input type="text" placeholder='enter name' /><br />
-    <select>
+  <form onSubmit={getFormData}>
+    <input type="text" placeholder='enter name' onChange={(e)=>setName(e.target.value)}/>
+    <br /> < br />
+    <select onChange={(e)=>setInterest(e.target.value)}>
       <option>Select Options</option>
       <option > Marvel</option>
       <option > DC Comics</option>
     </select>
-    <br></br>
-    <input type="checkbox" /><span>Accept terms and Conditions</span>
+    <br /> < br />
+    <input type="checkbox" onChange={(e)=>setTnc(e.target.checked)} /><span>Accept terms and Conditions</span>
+ 
+  <br /> <br />
+  <button type="submit">Submit</button>
   </form>
  </div>
+
   )
 }
 export default App;
