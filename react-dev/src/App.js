13863569e0ee98d9f677a0d8d9db58c3a3586792
prsndp <prasandeepp@gmail.com>
2022-10-23 18:31:54 +0530

    function passed as props

--- react-dev/src/App.js
+++ react-dev/src/App.js
@@ -1,35 +1,20 @@
 // import React from 'react';
 import React, {useState} from 'react';
 import './App.css';
+import Profile from './Profile';
+import User from './User';
+
 function App()
 {
-  const [name, setName] = useState("");
-  const [tnc, setTnc ] = useState(false);
-  const [interest, setInterest] = useState("");
-  function getFormData(e)
+  function getData()
   {
-    console.warn(name, tnc, interest);
-    e.preventDefault()
+    alert("hello from app");
   }
   return (
- <div className="App">
-  <h1>Handle Forms in React</h1>
-  <form onSubmit={getFormData}>
-    <input type="text" placeholder='enter name' onChange={(e)=>setName(e.target.value)}/>
-    <br /> < br />
-    <select onChange={(e)=>setInterest(e.target.value)}>
-      <option>Select Options</option>
-      <option > Marvel</option>
-      <option > DC Comics</option>
-    </select>
-    <br /> < br />
-    <input type="checkbox" onChange={(e)=>setTnc(e.target.checked)} /><span>Accept terms and Conditions</span>
- 
-  <br /> <br />
-  <button type="submit">Submit</button>
-  </form>
- </div>
-
+    <div className='App'>
+      <User data={getData} />
+    </div>
   )
 }
+
 export default App;
