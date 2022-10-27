d224ac24b92f0097974a1f8a46c24f98ffed9db8
prsndp <prasandeepp@gmail.com>
2022-10-27 22:57:46 +0530

    shouldComponentUpdate Learned today!

--- react-dev/src/App.js
+++ react-dev/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 
-
 class App extends React.Component {
 
 constructor()
 {
   super();
+  console.log("constructor")
   this.state={
-    name: "Prasandeep"
+    count: 0
   }
 }
-componentDidMount()
+shouldComponentUpdate()
 {
-  console.warn("componentDidMount");
+  console.log("ShouldComponentUpdate", this.state.count);
+  if(this.state.count > 5 && this.state.count < 10)
+  return true;
 }
 render()
 {
   console.warn("render")
   return (
     <div className='App'>
-      <h1>Component Did Mount</h1>
-      <button  onClick={()=>{this.setState({name: "Deep"})}}>Update Name</button>
+      <h1>Should Component Update: {this.state.count}</h1>
+      <button  onClick={()=>{this.setState({count: this.state.count+1})}}>Update Counter</button>
     </div>
     );
   }
