bab0c30ddc376fd12c36e0e1c973856f09afb6d0
prsndp <prasandeepp@gmail.com>
2022-11-06 23:53:05 +0530

    code changes done

--- react-dev/src/App.js
+++ react-dev/src/App.js
@@ -1,18 +1,29 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
-import User from  './User'
-
+import './style.css'
+import style from './custom.module.css'
+import {Button,Card } from 'react-bootstrap';
+import React from 'react';
 function App()
 {
-  const [data, setData] = useState(10);
-  const [count, setCount] = useState(100);
- 
-  return (
-    <div className='App'>
-      <User count = { count } data={data} />
-      <button onClick={()=>setCount(count+1)}> Update Counter </button>
-      <button onClick={()=>setData(data+1)}> Update Data</button>
-    </div>
-  ); 
+  return(
+
+  <div className='App'>
+    <h1>Install Bootstrap</h1>
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+    
+  </div>
+  );
 }
+
 export default App;
