b7ce393739eee4ddb941e7244d81f87cabe90d55
prsndp <prasandeepp@gmail.com>
2022-11-09 09:49:24 +0530

    react-router not wworking in this folder

--- react-dev/src/App.js
+++ react-dev/src/App.js
@@ -1,29 +1,15 @@
-import './App.css';
-import React, { useRef, useState } from 'react';
-
+import React from 'react';
+import {Routes, Route } from 'react-router-dom'; 
+import { Home } from './components/Home';
 function App()
 {
   return (
-    <div className='App'>
-      <h1>HOC</h1>
-      <HOCRed cmp={Counter}/>
-    </div>
-  );
-  function HOCRed(props)
-  {
-    return <h2 style={{backgroundColor: 'red', width: 100}}><props.cmp/></h2>
-  }
-  function Counter()
-  {
-    const [count, setCount] = useState(0);
-    return  (
-      <div>
-        <h3>{count}</h3>
-        <button onClick={()=> setCount(count+1)}>Update</button>
-      </div>
+    <Routes>
+      <Route path='/' element={<Home />}>
 
-    )
-  }
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
